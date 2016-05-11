ccc2803eff71edd2483e5d8ba4de0864e67f7135
Pedro Morales Rodriguez <Fyx@MacBook-Pro-de-Pedro.local>
2016-05-11 17:56:10 +0100

    Added index view and set up pug

---
+++ app.js
@@ -0,0 +1,11 @@
+// HTTP server stuff
+var express = require('express');
+var vue = require('vue');
+
+var app = new express();
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+	res.render('index')
+})
+app.listen(3000);
