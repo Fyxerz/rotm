09ca5d30df8a2f55812ae6ee1cf85410a2ee8a2f
Pedro Morales Rodriguez <Fyx@MBP-de-Pedro.home>
2016-05-12 13:27:31 +0100

    Set public folder and added main sass and css folders

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // HTTP server stuff
 var express = require('express');
-var vue = require('vue');
-var db = require('./posts');
+var db = require('./fakedb');
 
 var app = new express();
+
+// Define jade as view engine.
 app.set('view engine', 'pug');
 
+// Set static file path.
+app.static('/public/');
+
+// Get root view.
 app.get('/', function(req, res) {
-	res.render('index')
-})
+	res.render('index', {posts: db.getPosts()})
+});
+
+// Start server.
 app.listen(3000);
---
+++ fakedb.js
@@ -0,0 +1,24 @@
+var posts = [
+	{
+		name: "Post title",
+		content: "This is some content about the post",
+		author: "Pedro Morales Rodriguez",
+		votes: 0
+	},
+	{
+		name: "Post title",
+		content: "This is some content about the post",
+		author: "Pedro Morales Rodriguez",
+		votes: 0
+	},
+	{
+		name: "Post title",
+		content: "This is some content about the post",
+		author: "Pedro Morales Rodriguez",
+		votes: 0
+	}
+
+]
+exports.getPosts = function() {
+	return posts;
+}
